15774da888b7263186b4bf88d353642a32e47d45
Patrycja Godek <pdgodek04@gmail.com>
2022-10-20 17:08:38 +0200

    lab03
    zadania 1-10

--- mojaStronka/testowaAnkieta/admin.py
+++ mojaStronka/testowaAnkieta/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Osoba, Druzyna
+
+class OsobaAdmin(admin.ModelAdmin):
+    list_display = ['imie', 'nazwisko', 'druzyna', 'miesiac_urodzenia']
+    list_filter = ('druzyna', 'data_dodania')
+
+class DruzynaAdmin(admin.ModelAdmin):
+    list_display = ['nazwa', 'kraj']
+    list_filter = [('kraj')]
 
 admin.site.register(Question)
+admin.site.register(Osoba, OsobaAdmin)
+admin.site.register(Druzyna, DruzynaAdmin)
